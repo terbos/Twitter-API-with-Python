422ba1dce7b413e0ff3a1bc70730f76322c17d4b
Yiannis Papastergiou <terbos@users.noreply.github.com>
2020-05-11 19:55:23 +0100

    commit

---
+++ hidden.py
@@ -0,0 +1,12 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "YAHgUNi2GADljJWBug0J765F3",
+            "consumer_secret":
+            """QR7zW1lPv8dSYzZi7H55pB7FGTvbusSJLyx90CvP4D50DQt37J""",
+            "token_key": "229075576-zu4fLHLE64SbCEk0kM17fljT1KDDgOm7vZOwyaQc",
+            "token_secret": """Z5buZ8if45FIzJmbTFizMbX3NPEX3tv3cTFNfiqEfPoSD"""
+}
